5983fa408b43345d578509f6cb839e6e340c0bfa
Sihe Chen <chen.sihe1@northeastern.edu>
2025-03-02 23:30:55 -0500

    batch_size / render_chunk_size: 16384 -> 8192

--- internal/train_utils.py
+++ internal/train_utils.py
@@ -100,10 +100,12 @@ def compute_data_loss(batch, renderings, config):
         lossmult = torch.ones_like(lossmult)
 
     # convert back to sRGB and compute loss
-    batch_rgb = conversion.uniform_to_sRGB(batch['rgb'], config)
+    batch_rgb = batch['rgb']
+    # batch_rgb = conversion.uniform_to_sRGB(batch['rgb'], config)
 
     for rendering in renderings:
-        rendering_rgb = conversion.uniform_to_sRGB(rendering['rgb'], config)
+        rendering_rgb = rendering['rgb']
+        # rendering_rgb = conversion.uniform_to_sRGB(rendering['rgb'], config)
         resid_sq = (rendering_rgb - batch_rgb[..., :3]) ** 2
         denom = lossmult.sum()
         stats['mses'].append(((lossmult * resid_sq).sum() / denom).item())
